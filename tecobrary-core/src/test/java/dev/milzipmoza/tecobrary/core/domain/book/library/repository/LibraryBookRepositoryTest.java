f0dc939b9fde2cfa273304e609e032465f1c5a06
thedevluffy <thedevluffy@gmail.com>
2020-10-22 19:18:10 +0900

    test : library book repository and auditing test

---
+++ tecobrary-core/src/test/java/dev/milzipmoza/tecobrary/core/domain/book/library/repository/LibraryBookRepositoryTest.java
@@ -0,0 +1,44 @@
+package dev.milzipmoza.tecobrary.core.domain.book.library.repository;
+
+import dev.milzipmoza.tecobrary.core.domain.book.Book;
+import dev.milzipmoza.tecobrary.core.domain.book.library.entity.LibraryBook;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class LibraryBookRepositoryTest {
+
+    @Autowired
+    private LibraryBookRepository libraryBookRepository;
+
+    @Autowired
+    private EntityManager em;
+
+    @Test
+    void saveTest() {
+        LibraryBook libraryBook = new LibraryBook(Book.builder()
+                .title("제목")
+                .author("작가")
+                .publisher("출판사")
+                .isbn("isbn")
+                .description("요약")
+                .build());
+
+        LibraryBook savedLibraryBook = libraryBookRepository.saveAndFlush(libraryBook);
+
+        Long id = savedLibraryBook.getId();
+
+        em.clear();
+
+        LibraryBook foundLibraryBook = libraryBookRepository.findById(id).get();
+
+        assertThat(foundLibraryBook.getId()).isEqualTo(id);
+        assertThat(foundLibraryBook.getCreatedAt()).isNotNull();
+        assertThat(foundLibraryBook.getModifiedAt()).isNotNull();
+    }
+}
