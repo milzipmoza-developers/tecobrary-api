4303be97bd0c0d35d10df0eb29ac88ac7c46fefe
thedevluffy <thedevluffy@gmail.com>
2020-10-22 18:07:10 +0900

    init : spring boot project

---
+++ tecobrary-api/src/main/java/dev/milzipmoza/tecobrary/TecobraryApiApplication.java
@@ -0,0 +1,13 @@
+package dev.milzipmoza.tecobrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecobraryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecobraryApiApplication.class, args);
+	}
+
+}
