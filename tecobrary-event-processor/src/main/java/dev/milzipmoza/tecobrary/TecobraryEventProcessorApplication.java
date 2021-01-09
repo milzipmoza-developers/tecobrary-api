a5239b89ae4fd523171e11f76c045c1996936307
thedevluffy <thedevluffy@gmail.com>
2021-01-09 12:54:24 +0900

    init : event processor project

---
+++ tecobrary-event-processor/src/main/java/dev/milzipmoza/tecobrary/TecobraryEventProcessorApplication.java
@@ -0,0 +1,13 @@
+package dev.milzipmoza.tecobrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecobraryEventProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TecobraryEventProcessorApplication.class, args);
+    }
+
+}
