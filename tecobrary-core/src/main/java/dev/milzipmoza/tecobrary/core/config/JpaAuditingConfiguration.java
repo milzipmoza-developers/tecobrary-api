dafb9cbeaaaa853085bdf9bcd2a786c4c9b7aefc
thedevluffy <thedevluffy@gmail.com>
2021-02-05 16:56:24 +0900

    feat : implements jpa audit configuration

--- tecobrary-core/src/main/java/dev/milzipmoza/tecobrary/core/config/JpaAuditingConfiguration.java
+++ tecobrary-core/src/main/java/dev/milzipmoza/tecobrary/core/config/JpaAuditingConfiguration.java
@@ -1,18 +1,34 @@
 package dev.milzipmoza.tecobrary.core.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.context.SecurityContextHolder;
 
+import java.security.Principal;
 import java.util.Optional;
 
+@Slf4j
 @Configuration
 @EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 public class JpaAuditingConfiguration {
 
+    public static final String AUDITOR_SYSTEM = "SYSTEM";
+
     @Bean
     public AuditorAware<String> auditorProvider() {
-        return () -> Optional.of("임시");
+        return () -> Optional.of(
+                Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+                        .map(Principal::getName)
+                        .orElse(AUDITOR_SYSTEM)
+        );
+    }
+
+    @Bean
+    public AuditingEntityListener createAuditingListener() {
+        return new AuditingEntityListener();
     }
 }
