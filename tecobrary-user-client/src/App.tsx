f37800c18ca021c9a56b490b0f9a14fbcb8fbc5e
thedevluffy <thedevluffy@gmail.com>
2021-03-28 21:57:49 +0900

    chore : add styled components

--- tecobrary-user-client/src/App.tsx
+++ tecobrary-user-client/src/App.tsx
@@ -1,10 +1,34 @@
-import React, {ReactElement} from 'react';
+import React, {ReactElement} from "react";
+import styled from "styled-components";
+
+const Background = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  background-color: #ccc;
+`
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: white;
+  max-width: 36rem;
+  border: 1px solid black;
+`;
 
 function App(): ReactElement {
   return (
-    <div className="App">
-      새로운 앱
-    </div>
+    <Background className="background">
+      <Wrapper>
+        새로운 앱
+      </Wrapper>
+    </Background>
   );
 }
 
