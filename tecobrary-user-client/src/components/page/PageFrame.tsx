043c157e47857de79d394f26d032c483078a6abf
thedevluffy <thedevluffy@gmail.com>
2021-04-02 22:34:00 +0900

    style : edit page frame style

--- tecobrary-user-client/src/components/page/PageFrame.tsx
+++ tecobrary-user-client/src/components/page/PageFrame.tsx
@@ -8,15 +8,16 @@ interface Props {
 
 const PageWrapper = styled.div`
   width: auto;
-  padding: 1rem;
+  padding: 2rem 1rem 1rem 1rem;
   max-width: 36rem;
+  margin-bottom: 4rem; // for nav
 `
 
 const PageTitle = styled.div`
   margin-left: 0.5rem;
   margin-top: 1rem;
   font-weight: bold;
-  font-size: x-large;
+  font-size: xx-large;
 `
 
 function PageFrame({title, children}: Props): ReactElement {
