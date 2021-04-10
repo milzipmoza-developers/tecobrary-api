caedac7e4bf94736dd9963bca603e1453e7ce69d
thedevluffy <thedevluffy@gmail.com>
2021-04-10 16:07:44 +0900

    feat : add fontWeight props and change html tag

--- tecobrary-user-client/src/components/badges/CategoryBadge.tsx
+++ tecobrary-user-client/src/components/badges/CategoryBadge.tsx
@@ -1,22 +1,24 @@
 import {ReactElement, ReactNode} from "react";
 import styled from "styled-components";
+import {Property} from "csstype";
 
 interface Props {
+  fontWeight?: Property.FontWeight
   backgroundColor: string
   children: string
 }
 
-export const CategoryBadge = ({backgroundColor, children}: Props): ReactElement => {
+export const CategoryBadge = ({fontWeight, backgroundColor, children}: Props): ReactElement => {
   return (
     <Wrapper style={{backgroundColor: backgroundColor}}>
-      <BadgeText>
+      <BadgeText style={{fontWeight}}>
         {children}
       </BadgeText>
     </Wrapper>
   )
 }
 
-const Wrapper = styled.div`
+const Wrapper = styled.span`
   height: fit-content;
   width: fit-content;
   padding: 0.1rem 0.2rem 0.1rem 0.2rem;
@@ -24,7 +26,7 @@ const Wrapper = styled.div`
   border-radius: 0.3rem;
 `
 
-const BadgeText = styled.div`
+const BadgeText = styled.a`
   width: fit-content;
   height: fit-content;
   font-size: small;
